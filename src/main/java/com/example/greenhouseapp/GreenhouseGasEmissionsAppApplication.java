154e2e3b6196ed5370e43a6fe534542095cef613
Abdul Badmos <129188834+adebadd@users.noreply.github.com>
2024-11-27 09:53:06 +0000

    Project Setup
    - Installed STS4/Spring Tools 4 from eclipse market
    - Created a new spring starter project in eclipse under "Spring Boot"

---
+++ src/main/java/com/example/greenhouseapp/GreenhouseGasEmissionsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.greenhouseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenhouseGasEmissionsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenhouseGasEmissionsAppApplication.class, args);
+	}
+
+}
